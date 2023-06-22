236801328fa5e5f83171aef758610ac5834474bf
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2023-06-22 03:32:16 +0000

    feat: better navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="navbar fixed-bottom navbar-light bg-light">
     <div class="container-fluid justify-content-around">
-      <router-link to="" class="link-dark" @click.prevent="logout">
-        <BIconDoorOpen width="25" height="25" />
+      <router-link to="/home" class="link-dark">
+        <BIconHouseDoor width="32" height="32" />
       </router-link>
       <router-link to="/records" class="link-dark">
         <BIconList width="32" height="32" />
       </router-link>
-      <router-link to="/home" class="link-dark">
-        <BIconHouseDoor width="32" height="32" />
+      <router-link to="" class="link-dark" @click.prevent="logout">
+        <BIconDoorOpen width="25" height="25" />
       </router-link>
     </div>
   </nav>
